d1c6944ac0234030db8cacd6ab1d77b9dece113f
gootibi <gootibi@gmail.com>
2024-03-26 17:47:08 +0100

    Style the Issue Detail Page - add Radix-UI themes: Heading, Flex, Text, Card ect.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
@@ -18,10 +20,14 @@ const IssueDetailPage = async ({ params }: Props) => {
 
     return (
         <div>
-            <p>{issue.title}</p>
-            <p>{issue.status}</p>
-            <p>{issue.createdAt.toDateString()}</p>
-            <p>{issue.description}</p>
+            <Heading as='h1'>{issue.title}</Heading>
+            <Flex className='gap-3' my="2">
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card>
+                <p>{issue.description}</p>
+            </Card>
         </div>
     )
 }
