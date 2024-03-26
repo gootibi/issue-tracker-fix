0ff779b793e514176aa19b436d8e561913b62b5d
gootibi <gootibi@gmail.com>
2024-03-26 22:14:37 +0100

    Refoactor: Apply the Single Responsibility Principle (SRP) - refactor data and edit button 2 local component files

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,19 @@
+import { IssueStatusBadge } from '@/app/components'
+import { Issue } from '@prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import ReactMarkdown from 'react-markdown'
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+    return (
+        <>
+            <Heading as='h1'>{issue.title}</Heading><Flex className='gap-3' my="2">
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex><Card className='prose' my="4">
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
+        </>
+    )
+}
+
+export default IssueDetails
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,9 @@
-import { IssueStatusBadge } from '@/app/components/'
 import prisma from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
-import { notFound } from 'next/navigation'
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
+import { Box, Grid } from '@radix-ui/themes'
 import delay from 'delay'
-import Link from 'next/link'
-import { Pencil2Icon } from '@radix-ui/react-icons'
+import { notFound } from 'next/navigation'
+import EditIssueButton from './EditIssueButton'
+import IssueDetails from './IssueDetails'
 
 interface Props {
     params: { id: string }
@@ -28,22 +25,10 @@ const IssueDetailPage = async ({ params }: Props) => {
     return (
         <Grid columns={{ initial: "1", md: "2" }} gap="5"> {/* https://www.radix-ui.com/themes/docs/theme/breakpoints  initial (phone size 0px)-> 1 columns, md (tablet size 1024px) 2 columns*/}
             <Box>
-                <Heading as='h1'>{issue.title}</Heading>
-                <Flex className='gap-3' my="2">
-                    <IssueStatusBadge status={issue.status} />
-                    <Text>{issue.createdAt.toDateString()}</Text>
-                </Flex>
-                <Card className='prose' my="4">
-                    <ReactMarkdown>{issue.description}</ReactMarkdown>
-                </Card>
+                <IssueDetails issue={issue} /> {/* This row add issue data - from IssueDetails.tsx */}
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon />
-                    <Link href={`/issues/${issue.id}/edit`}>
-                        Edit issue
-                    </Link>
-                </Button>
+                <EditIssueButton issueId={issue.id} /> {/* This row add "Edit issue" button - from EditIssueButton.tsx */}
             </Box>
         </Grid>
     )
