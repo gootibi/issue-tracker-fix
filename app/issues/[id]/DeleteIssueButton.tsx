2344061b38c7bdf38cb55038ed624c0bad2d383b
gootibi <gootibi@gmail.com>
2024-03-28 01:31:19 +0100

    Adding a Confirmation Dialog Box - Radix-ui AlertDialog - Delete Issue Button adding Dialog Box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,10 +1,39 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      {/* Alert dialog box triggerer - button (yet Delete Issue) */}
+      <AlertDialog.Trigger>
+        <Button color='red'>
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+
+      <AlertDialog.Content>
+        {/* Alert dialog box "title" and "description" */}
+        <AlertDialog.Title>
+          Confirm Deletion
+        </AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this issue? This action is permanent and
+          cannot be undone.
+        </AlertDialog.Description>
+        {/* Alert dialog box have two button - cancel and action button */}
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant='soft' color='gray'>Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant='solid' color='red'>Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+
+      </AlertDialog.Content>
+
+    </AlertDialog.Root>
   )
 }
 
