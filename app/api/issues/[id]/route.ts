cae080c10573263672a5ae937fadb8cb142c5f71
gootibi <gootibi@gmail.com>
2024-03-28 14:32:02 +0100

    Building an API for deleting issues- add in route.ts DELETE function

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -7,21 +7,26 @@ export async function PATCH(
     { params }: { params: { id: string } }) {
 
     const body = await request.json();
+
     // Validation de body data
     const validation = issueSchema.safeParse(body)
+
     // if the data is not valid return the error
     if (!validation.success) {
         return NextResponse.json(validation.error.format(), { status: 400 })
     }
+
     // Search in database with params ID
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
     })
-    // The ID not found in database, return the error 'Invalid ID os issue', issue not found
+
+    // The ID not found in database, return the error 'Invalid ID, issue', issue not found
     if (!issue) {
         return NextResponse.json({ error: 'Invalid issue' }, { status: 404 })
     }
-    // Update de database whit prisma
+
+    // Update the database whit prisma
     const updatedIssue = await prisma.issue.update({
         where: { id: issue.id },
         data: {
@@ -31,4 +36,26 @@ export async function PATCH(
     })
     // The issue is valid, return the issue data
     return NextResponse.json(updatedIssue)
+}
+
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: { id: string } }) {
+        
+    // Search in database with params ID
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) }
+    })
+
+    // The ID not found in database, return the error 'Invalid ID, issue', issue not found
+    if (!issue) {
+        return NextResponse.json({ error: 'Invalid issue' }, { status: 404 })
+    }
+
+    // Delete issue in database whit prisma
+    await prisma.issue.delete({
+        where: { id: issue.id }
+    })
+
+    return NextResponse.json({})
 }
