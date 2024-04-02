5674b8b1a9c9410e21f7057725cf7f951220136e
gootibi <gootibi@gmail.com>
2024-04-03 01:27:34 +0200

    Sorting Issues - Validation and set the orderBy property -> add this property on prisma findMeny method

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -29,9 +29,17 @@ const IssuePage = async ({ searchParams }: Props) => {
   // validation: statuses includes status then search params, or not including then undefined -> all data find and load 
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
 
-  // When status undefined loading all data
+  // Validation and set the orderBy property, when searchParams.orderBy includes columns array values set { [searchParams.orderBy]: 'asc' } or not -> undefined
+  const orderBy = columns
+    .map(column => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined
+
+  // When status undefined loading all data, and add the orderBy property
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy
   })
 
   // Delay - simulate the slow server - test loading skeleton
